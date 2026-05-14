2d5ba072b5363e1ccf0c66f3328a98a77463ff83
Rocky <72786003+rocky2210@users.noreply.github.com>
2026-05-14 18:38:21 +0530

    getting started

---
+++ seleniumJavaTest/src/test/java/gettingStarted/SeleniumTest.java
@@ -0,0 +1,11 @@
+package gettingStarted;
+
+public class SeleniumTest {
+
+    public static void main(String[] args){
+        System.out.println("Hello world");System.out.println("Hello world");
+
+
+
+    }
+}
