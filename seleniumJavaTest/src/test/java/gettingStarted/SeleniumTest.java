04535d29802d55a5862ad9411c9868f3dd99e244
Rocky <72786003+rocky2210@users.noreply.github.com>
2026-05-15 12:27:32 +0530

    Basic Web Driver selenium manage/browser drivers

--- seleniumJavaTest/src/test/java/gettingStarted/SeleniumTest.java
+++ seleniumJavaTest/src/test/java/gettingStarted/SeleniumTest.java
@@ -13,7 +13,29 @@ public class SeleniumTest {
     WebDriver driver = new ChromeDriver();
 
     // 3. Navigate to a specific URL
-    driver.navigate().to("https://google.com");
+    driver.navigate().to("https://bing.com");
+
+    // Set the screen to full screen
+    driver.manage().window().fullscreen();
+
+    // minimize the screen of the browser
+    driver.manage().window().minimize();
+
+    // Close the browser
+//    driver.close();
+
+    System.out.println("The URL on my Browser: "+ driver.getCurrentUrl());
+
+    // Get the current page source
+    String pageSource = driver.getPageSource();
+
+    if(pageSource.contains("Search with Microsoft Bing and use the power of AI to find information, explore webpages, images, videos, maps, and more. A smart search engine for the forever curious.")){
+        System.out.println("True");
+    }else{
+        System.out.println("False");
+    }
+
+    System.out.println("My browser current handle: "+ driver.getWindowHandle());
 
     }
 }
