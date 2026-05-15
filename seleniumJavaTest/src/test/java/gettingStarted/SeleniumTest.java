11f4afe617bbc94bcc2e44c768033f8d57abdae7
Rocky <72786003+rocky2210@users.noreply.github.com>
2026-05-15 11:22:34 +0530

    Basic Driver Setup

--- seleniumJavaTest/src/test/java/gettingStarted/SeleniumTest.java
+++ seleniumJavaTest/src/test/java/gettingStarted/SeleniumTest.java
@@ -1,11 +1,19 @@
 package gettingStarted;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class SeleniumTest {
 
     public static void main(String[] args){
-        System.out.println("Hello world");System.out.println("Hello world");
+//        System.out.println("Hello world");
 
+    // 1. Create a webdriver with chromeDriver object
+    // 2. Launch browser
+    WebDriver driver = new ChromeDriver();
 
+    // 3. Navigate to a specific URL
+    driver.navigate().to("https://google.com");
 
     }
 }
